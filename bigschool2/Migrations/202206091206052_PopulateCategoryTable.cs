c7fe487c7ed189a00da18fcd2f5e0366124576b5
thuypq123 <lmthuy37@gmail.com>
2022-06-09 22:06:32 +0700

    Add project files.

---
+++ bigschool2/Migrations/202206091206052_PopulateCategoryTable.cs
@@ -0,0 +1,21 @@
+﻿namespace bigschool2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT categories ON");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1, 'Development')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2, 'Business')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3, 'Marketing')");
+            Sql("SET IDENTITY_INSERT categories OFF");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ bigschool2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bigschool2.Startup))]
+namespace bigschool2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
